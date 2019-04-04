7b90fd2497c7971ed890f990a79941423fd5de5c
Garry Marsland <garry@basefour.co.uk>
2019-04-04 21:24:38 +0100

    Adds GenericHost logging example

---
+++ 2.2/Logging/ConsoleApp/GenericHost/ExampleLoggingBackgroundService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace StarterPacks.Logging.GenericHost
+{
+    public class ExampleLoggingBackgroundService : BackgroundService
+    {
+        private readonly ILogger<ExampleLoggingBackgroundService> _logger;
+
+        public ExampleLoggingBackgroundService(ILogger<ExampleLoggingBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogTrace("Logging trace-level event");
+                _logger.LogDebug("Logging debug-level event");
+                _logger.LogInformation("Logging information-level event");
+                _logger.LogWarning("Logging warning-level event");
+                _logger.LogError("Logging error-level event");
+                _logger.LogCritical("Logging critical-level event");
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
---
+++ 2.2/Logging/ConsoleApp/GenericHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace StarterPacks.Logging.GenericHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddConsole(consoleLoggerOptions =>
+                    {
+                        consoleLoggerOptions.DisableColors = false;
+                    });
+                    loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+                })
+                .ConfigureServices(serviceCollection =>
+                {
+                    serviceCollection.AddHostedService<ExampleLoggingBackgroundService>();
+                });
+
+            var host = hostBuilder.Build();
+
+            await host.RunAsync();
+        }
+    }
+}
