8950ba7b15c81099a9faa6d3376d760ebef99af6
Garry Marsland <garry@basefour.co.uk>
2019-04-30 22:51:56 +0100

    Add basic configuration version

---
+++ 2.2/Configuration/ConsoleApp/Basic/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace StarterPacks.Configuration.Basic
+{
+    public class AppSettings
+    {
+        public string Hello { get; set; }
+
+        public NestedSettings NestedSettings { get; set; }
+    }
+
+    public class NestedSettings
+    {
+        public string Value1 { get; set; }
+        public int Value2 { get; set; }
+    }
+}
---
+++ 2.2/Configuration/ConsoleApp/Basic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace StarterPacks.Configuration.Basic
+{
+    class Program
+    {
+        static void Main()
+        {
+            var environment = "development";
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{environment}.json", false);
+
+            var configuration = configurationBuilder.Build();
+
+            var appSettings = new AppSettings();
+            configuration.Bind(appSettings);
+
+            Console.WriteLine("Reading from configuration: " + appSettings.Hello + " - " + appSettings.NestedSettings.Value1 + " - " + appSettings.NestedSettings.Value2);
+            Console.ReadKey();
+        }
+    }
+}
